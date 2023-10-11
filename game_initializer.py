5b95a23a3f1dfb1234c41b20097c2491e5dfde68
Equinoxxe <unconfigured@null.spigotmc.org>
2023-10-11 00:36:41 -0500

    Hopefully 60fps

--- game_initializer.py
+++ game_initializer.py
@@ -5,6 +5,7 @@ import time
 a_key = keymask(Keys.KEY_A)
 b_key = keymask(Keys.KEY_B)
 
+target_fps = 60
 
 def run_game():
     emu = DeSmuME()
@@ -14,21 +15,22 @@ def run_game():
     window = emu.create_sdl_window()
 
     # Run the game at 60fps
-    frame_num = 0
+    frame_interval = 1.0 / target_fps
     while not window.has_quit():
+        # Record the start time of the current frame
+        start_time = time.time()
+
         window.process_input()  # Controls are the default DeSmuME controls, see below.
         emu.cycle()
         window.draw()
 
-        if frame_num % 2:
-            emu.input.keypad_add_key(a_key)
-            emu.input.keypad_add_key(b_key)
-        else:
-            emu.input.keypad_rm_key(a_key)
-            emu.input.keypad_rm_key(b_key)
+        # Calculate the time taken to process the frame
+        elapsed_time = time.time() - start_time
 
-        frame_num += 1
-        time.sleep(1/60)
+        # If the processing time is less than the frame interval,
+        # sleep for the remaining time to achieve the target fps
+        if elapsed_time < frame_interval:
+            time.sleep(frame_interval - elapsed_time)
 
 
 if __name__ == "__main__":
